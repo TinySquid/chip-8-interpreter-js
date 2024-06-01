3579dbf595df846385ee20a185f1a5be2f93488c
Michael Nunes <tinysquid.git@gmail.com>
2024-05-31 23:26:44 -0600

    nothing to see here

---
+++ src/js/typedefs.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} Chip8InterpreterOptions
+ * @property {number} cyclesPerSecond
+ * @property {number} chunkIntervalMs
+ * @property {number} programStartAddress
+ * @property {number} memorySize
+ * @property {number} stackSize
+ */
