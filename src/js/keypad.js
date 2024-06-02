e3368e88800fb400b1832937b9742b2d83a3b660
Michael Nunes <tinysquid.git@gmail.com>
2024-06-01 23:39:39 -0600

    add keypad with remapped bindings

--- src/js/keypad.js
+++ src/js/keypad.js
@@ -1,1 +1,65 @@
-export default class Keypad {}
+export default class Keypad {
+  constructor() {
+    this.keybinds = {
+      Digit1: 0x1,
+      Digit2: 0x2,
+      Digit3: 0x3,
+      Digit4: 0xc,
+      KeyQ: 0x4,
+      KeyW: 0x5,
+      KeyE: 0x6,
+      KeyR: 0xd,
+      KeyA: 0x7,
+      KeyS: 0x8,
+      KeyD: 0x9,
+      KeyF: 0xe,
+      KeyZ: 0xa,
+      KeyX: 0x0,
+      KeyC: 0xb,
+      KeyV: 0xf,
+    };
+
+    this.keyStates = {};
+
+    // for handling FX0A instruction
+    this.blockingKeyPressHandler = null;
+
+    Object.values(this.keybinds).forEach((v) => {
+      this.keyStates[v] = false;
+    });
+
+    window.addEventListener("keydown", this.onKeyDown.bind(this), false);
+    window.addEventListener("keyup", this.onKeyUp.bind(this), false);
+  }
+
+  isKeyPressed(code) {
+    return !!this.keyStates[code];
+  }
+
+  /**
+   * @param {KeyboardEvent} event
+   */
+  onKeyDown(event) {
+    const key = this.keybinds[event.code];
+
+    if (key !== undefined) {
+      this.keyStates[key] = true;
+
+      if (this.blockingKeyPressHandler !== null) {
+        this.blockingKeyPressHandler(key);
+        this.blockingKeyPressHandler = null;
+      }
+    }
+  }
+
+  /**
+   * @param {KeyboardEvent} event
+   */
+  onKeyUp(event) {
+    const key = this.keybinds[event.code];
+
+    if (key !== undefined) {
+      this.keyStates[key] = false;
+    }
+  }
+}
