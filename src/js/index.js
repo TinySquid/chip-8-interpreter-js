5586f14d820450dd50a29baa0b11d676f421188c
Michael Nunes <tinysquid.git@gmail.com>
2024-06-15 01:16:13 -0600

    messing around with the emulator controls styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,57 +1,71 @@
-// import Chip8 from "./chip8";
-// import Keypad from "./keypad";
-// import Renderer from "./renderer";
-// import Speaker from "./speaker";
+import Chip8 from "./chip8";
+import Keypad from "./keypad";
+import Renderer from "./renderer";
+import Speaker from "./speaker";
 
-// import fontData from "./font";
-// import { readROM } from "./utils";
+import fontData from "./font";
+import { readROM } from "./utils";
 
-// /** @type {Chip8InterpreterOptions} */
-// const options = {
-//   cyclesPerSecond: 2000,
-//   chunkIntervalMs: 100,
-//   programStartAddress: 0x200,
-//   defaultFontStartAddress: 0x050,
-//   memorySize: 4096,
-// };
+/** @type {Chip8InterpreterOptions} */
+const options = {
+  cyclesPerSecond: 2000,
+  chunkIntervalMs: 100,
+  programStartAddress: 0x200,
+  defaultFontStartAddress: 0x050,
+  memorySize: 4096,
+};
 
-// const renderer = new Renderer(64, 32, 10, "renderer");
-// const keypad = new Keypad();
-// const speaker = new Speaker();
+const renderer = new Renderer(64, 32, 10, "renderer");
+const keypad = new Keypad();
+const speaker = new Speaker();
 
-// const chip8 = new Chip8(renderer, keypad, speaker, options);
+const chip8 = new Chip8(renderer, keypad, speaker, options);
 
-// chip8.loadFont(options.defaultFontStartAddress, fontData);
+chip8.loadFont(options.defaultFontStartAddress, fontData);
 
-// const startBtn = document.getElementById("start-btn");
-// startBtn.addEventListener("click", () => {
-//   if (!chip8.isRunning) {
-//     chip8.start();
-//   }
-// });
+chip8.renderer.togglePixel(10, 10);
 
-// const stopBtn = document.getElementById("stop-btn");
-// stopBtn.addEventListener("click", () => {
-//   if (chip8.isRunning) {
-//     chip8.stop();
-//   }
-// });
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", () => {
+  if (!chip8.isRunning) {
+    chip8.start();
 
-// const stepBtn = document.getElementById("step-btn");
-// stepBtn.addEventListener("click", () => {
-//   if (!chip8.isRunning) {
-//     chip8.step();
-//   }
-// });
+    startBtn.setAttribute("disabled", true);
+    stepBtn.setAttribute("disabled", true);
 
-// const loadBtn = document.getElementById("load-btn");
-// loadBtn.addEventListener("click", async () => {
-//   chip8.reset();
+    stopBtn.removeAttribute("disabled");
+  }
+});
 
-//   const buffer = await readROM("/games/cavern");
+const stopBtn = document.getElementById("stop-btn");
+stopBtn.setAttribute("disabled", true);
 
-//   chip8.loadROM(options.programStartAddress, buffer);
+stopBtn.addEventListener("click", () => {
+  if (chip8.isRunning) {
+    chip8.stop();
 
-//   // audio in browser can only be started after a user event
-//   speaker.init();
-// });
+    startBtn.removeAttribute("disabled");
+    stepBtn.removeAttribute("disabled");
+
+    stopBtn.setAttribute("disabled", true);
+  }
+});
+
+const stepBtn = document.getElementById("step-btn");
+stepBtn.addEventListener("click", () => {
+  if (!chip8.isRunning) {
+    chip8.step();
+  }
+});
+
+const loadBtn = document.getElementById("load-btn");
+loadBtn.addEventListener("click", async () => {
+  chip8.reset();
+
+  const buffer = await readROM("/games/ibm");
+
+  chip8.loadROM(options.programStartAddress, buffer);
+
+  // audio in browser can only be started after a user event
+  speaker.init();
+});
